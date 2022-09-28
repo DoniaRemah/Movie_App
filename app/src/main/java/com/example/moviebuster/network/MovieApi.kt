05717d9a358ad6f8132f2b9107abf3fa9e3d32c9
Donia Remah <doniaremah@gmail.com>
2022-09-28 16:19:56 +0200

    Solved Warnings, separated Functions in activities, created general data classes for api response.

--- app/src/main/java/com/example/moviebuster/network/MovieApi.kt
+++ app/src/main/java/com/example/moviebuster/network/MovieApi.kt
@@ -3,7 +3,6 @@ package com.example.moviebuster.network
 import com.example.moviebuster.model.*
 import retrofit2.http.GET
 import retrofit2.http.Query
-import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.Path
 
